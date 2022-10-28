3aee16b51fa408ba162c1679bdc0dcb7c4f73381
Orest <galenzao@gmail.com>
2022-10-28 13:45:56 +0300

    Comit

---
+++ script/language.js
@@ -0,0 +1,191 @@
+const langArr = {
+    title: {
+        en:"OGshadoww | portfolio",
+        ua:"OGshadoww | Портфоліо"
+    },
+    beginTitle : {
+        en:"Hello i’m",
+        ua:"Привіт я"
+    },
+    beginName : {
+        en:"Orest Galenza",
+        ua:"Галенза Орест"
+    },
+    beginDescription : {
+        en:"frontend developer",
+        ua:"Фронтенд розробник"
+    },
+    beginScroll: {
+        en:"Scroll down",
+        ua:"Гортай униз"
+    },
+    beginButton1 : {
+        en: "look my portfolio",
+        ua:"Моє портфоліо"
+    },
+    buttonFill : {
+        en:"contact me",
+        ua:"Зв'язатись зі мною"
+    },
+    aboutTitle : {
+        en:"About me",
+        ua:"Про мене"
+    },
+    aboutSubtitle : {
+        en:"Get to know",
+        ua:"Дізнайся"
+    },
+    aboutCardTitle1: {
+        en:"Feedback",
+        ua:"Зворотній зв'язок"
+    },
+    aboutCardText1: {
+        en:"you can contact me at a time convenient for you",
+        ua:"ви можете зв'язатися зі мною в зручний для вас час"
+        
+    },
+    aboutBtn: {
+        en:"Contact me",
+        ua:"Звязатись зі мною"
+    },
+    experienceTitle: {
+        en:"My experience",
+        ua:"Мій досвід"
+    },
+    experienceSubtitle: {
+        en:"What Skills I Have",
+        ua:"Якими навичками я володію"
+    },  
+    experienceItemTitle1: {
+        en:"Frontend Development",
+        ua:"Фронтенд розробка"
+    },
+    experienceItemTitle2 :{
+        en:"Coming soon",
+        ua:"Скоро"
+    },
+    experienceItemSkillSoon1: {
+        en:"Coming soon",
+        ua:"Скоро"
+    },
+    experienceItemSkillSoon2: {
+        en:"Coming soon",
+        ua:"Скоро"
+    },
+    comingSoon: {
+        en:"Coming soon",
+        ua:"Скоро"
+    },
+    servicesTitle : {
+        en:"Services",
+        ua:"Послуги"
+    },
+    servicesSubtitle : {
+        en:"What I Offer",
+        ua:"Що я пропоную"
+    },
+    servicesItemTitle : {
+        en:"Web Develpoment",
+        ua:"Веб розробка"
+    },
+    portfolioTitle : {
+        en:"Portfolio",
+        ua:"Портфоліо"
+    },
+    portfolioSubtitle : {
+        en:"My Recent Work",
+        ua:"Моя останні роботи"
+    },
+    PortfolioItemTitle1 : {
+        en:"Crypto Company ToTheMoon & trading courses",
+        ua:"Криптокомпанія ToTheMoon & торгові курси"
+    },
+    PortfolioItemTitle2 : {
+        en:"Quiz app on JavaScript",
+        ua:"Програма опитування на JavaScript"
+    },
+    testimonialsTitle: {
+        en:"Testimonials",
+        ua:"Відгуки"
+    },
+    testimonialsSubtitle : {
+        en:"Rewievs from client",
+        ua:"Відгуки від клієнтів"
+    },
+    contactTitle : {
+        en:"Contact Me",
+        ua:"Звязатись зі мною"
+    },
+    contactSubtitle : {
+        en:"Get In Touch",
+        ua:"Зв'язатись"
+    },
+    contactInputName : {
+        en:"Your full name",
+        ua:"Ваше повне ім'я"
+    },
+    contactInputEmail : {
+        en:"Your Email",
+        ua:"Ваша електронна пошта"
+    },
+    contactInputMessage : {
+        en:"Your message",
+        ua:"Ваше повідомлення"
+    },
+    contactBtn : {
+        en:"Send message",
+        ua:"Надіслати повідомлення"
+    },
+    contactCardMessage1 : {
+        en:"Send a message",
+        ua:"Надіслати повідомлення"
+    },
+    contactCardMessage2 : {
+        en:"Send a message",
+        ua:"Надіслати повідомлення"
+    },
+    contactCardCallTitle : {
+        en:"Phone",
+        ua:"Телефон"
+    },
+    contactCardCall : {
+        en:"Call",
+        ua:"Телефонуйте"
+    },
+    footerTitle : {
+        en:"Galenza Orest",
+        ua:"Галенза Орест"
+    },
+    footerMenuItem1 : {
+        en:"Home",
+        ua:"Головна"
+    },
+    footerMenuItem2 : {
+        en:"About me",
+        ua:"Про мене"
+    },
+    footerMenuItem3 : {
+        en:"Experience",
+        ua:"Досвід"
+    },
+    footerMenuItem4 : {
+        en:"Services",
+        ua:"Послуги"
+    },
+    footerMenuItem5 : {
+        en:"Portfolio",
+        ua:"Портфоліо"
+    },
+    footerMenuItem6 : {
+        en:"Testimonials",
+        ua:"Відгуки"
+    },
+    footerMenuItem7 : {
+        en:"Contact",
+        ua:"Контакти"
+    },
+    footerCopyright : {
+        en:"©Orest Galenza. All Right Reserved.",
+        ua:"©Орест Галенза. Усі права захищено."
+    }
+}
---
+++ script/main.js
@@ -0,0 +1,139 @@
+// use IntersectionObserver for create animation with scroll
+
+const animItem = document.querySelectorAll('.anim__item') // create variable for all element what must be animated
+
+function onEntry (entry) {
+    entry.forEach(change => {
+        if(change.isIntersecting) {
+            change.target.classList.add('anim_show')
+        }   //else {
+            // if(!change.target.classList.contains('anim_disposable')) {
+            //     change.target.classList.remove('anim_show')
+            // }
+        //}
+    });
+}
+
+let options = {
+    threshold:[0.5] // create options
+}
+
+let observer = new IntersectionObserver(onEntry, options)
+
+for(let el of animItem) {
+    observer.observe(el)
+}
+
+// use Swiper.js for create slider
+
+new Swiper('.swiper-conteiner', {
+    pagination: {
+        el:'.swiper-pagination' // create pegitation
+    },
+    navigation: {
+        nextEl: '.swiper-button-next', // create button for slide next
+        prevEl: '.swiper-button-prev' // create button for slide prev
+    },
+    loop:true
+})
+
+// Do link for button, becouse cant do this with using tag <a>
+
+const portfolioBtn = document.querySelector('.hvr-bob') // create variable for scroll button to portfolio
+const contactBtn = document.querySelectorAll('.hvr-radial-out') // create variable for scrotll button to contact
+const contactBox = document.querySelector('.contact') // create variable for contact
+const portfolioBox = document.querySelector('.portfolio') // create variable for portfolio
+
+portfolioBtn.addEventListener('click', () => { // add event for scroll
+    portfolioBox.scrollIntoView() // scroll to element
+})
+
+contactBtn.forEach(btn => {
+    btn.addEventListener('click', () => { // add event for scroll
+        contactBox.scrollIntoView() // scroll to element
+    })
+})
+
+// form validate
+
+const form = document.querySelector('.form')  // create variable for form
+const input = document.querySelectorAll('.form__input') // create variable for input
+
+let formEmail = document.querySelector('.form__email')
+
+form.addEventListener('submit', e => { // add event submit 
+    input.forEach(item => {
+        if(item.value == '') { // 
+            item.classList.add('error') // if input clear add class error
+            e.preventDefault()
+        }   
+        else {
+            item.classList.remove('error')
+        }
+        
+    let r = /^[\w\.\d-_]+@[\w\.\d-_]+\.\w{2,4}$/i; // create regularEx for validate email
+
+    if(!r.test(formEmail.value)) {
+        formEmail.classList.add('error')
+        e.preventDefault()
+    }
+    })
+})
+
+// open about card
+const cardBox = document.querySelectorAll('.about__content__cards__item')
+const cardContent = document.querySelectorAll('.about__content__cards__item__content')
+
+cardBox.forEach(card => {
+    card.addEventListener('click', () => {
+        let cardId = card.getAttribute('data-set')
+        let currentContent = document.getElementById(cardId)
+
+        cardBox.forEach(item => {
+            if(!item.classList.contains('active')) {
+                item.classList.remove('active')
+            }
+        })
+        cardContent.forEach(item => {
+            if(!item.classList.contains('active')) {
+                item.classList.remove('active')
+            }
+        })
+        card.classList.toggle('active') 
+        currentContent.classList.toggle('active')
+    })
+})
+
+// change language 
+const ua = document.querySelector("#ua")
+const en = document.querySelector("#en")
+const allLang = ['ua', 'en']
+
+ua.addEventListener('click', changeUrlLanguage)
+en.addEventListener('click', changeUrlLanguage)
+
+// redirection url
+function changeUrlLanguage() {
+    let lang = this.textContent.toLowerCase()
+    location.href = `${window.location.pathname}#${lang}`
+    location.reload()
+}
+
+function changeLanguage () {
+    let hash = window.location.hash
+    hash = hash.substr(1)
+    console.log(hash)
+    if(!allLang.includes(hash)) {
+        location.href = window.location.pathname + '#en'
+        location.reload()
+    }
+    document.querySelector('title').innerHTML = langArr['title'][hash]
+    for(let key in langArr) {
+        let elem = document.querySelector('.lng-' + key)
+        if(elem) {
+            elem.innerHTML = langArr[key][hash]
+        }
+    }
+}
+
+changeLanguage()
